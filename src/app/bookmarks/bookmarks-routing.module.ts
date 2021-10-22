d4d8f193710f366357abcea2753c72f6f67acc6c
Allain John Notarte <itsallain@gmail.com>
2021-10-22 22:35:11 +0800

    added explore modal component

---
+++ src/app/bookmarks/bookmarks-routing.module.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router"
+import { BookmarksComponent } from "./bookmarks.component";
+
+export const routes: Routes = [
+    {
+        path: 'bookmarks',
+        component: BookmarksComponent
+    }
+];
